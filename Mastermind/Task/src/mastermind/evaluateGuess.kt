5f063609981ed932d5ad036e71991bb18c68567b
kefasb <kefasb@gmail.com>
2023-05-17 11:37:48 +0200

    First solution slightly improved.

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -13,18 +13,16 @@ fun evaluateGuess(secret: String, guess: String): Evaluation {
         val (leftSecret, leftGuess) = secret.zip(guess).filterNot { pair -> pair.first == pair.second }
             .unzip()
         val leftSecretMutable = ArrayList(leftSecret)
-        var wrong = 0
 
         for (char in leftGuess) {
-            if (leftSecretMutable.remove(char)) {
-                wrong++
-            }
+            leftSecretMutable.remove(char)
         }
 
-        return wrong
+        return leftSecret.count() - leftSecretMutable.count()
     }
 
     return Evaluation(
         evaluateRight(secret, guess),
-        evaluateWrong(secret, guess))
+        evaluateWrong(secret, guess)
+    )
 }
